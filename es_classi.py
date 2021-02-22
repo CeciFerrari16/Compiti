3dde3868e3bb5d13c07839254e6523c807dce5d5
CeciFerrari16 <cecilia.ferrari2@liceoariostospallanzani-re.edu.it>
2021-02-22 12:48:56 +0100

    Aggiornato es_classi

--- es_classi.py
+++ es_classi.py
@@ -1,20 +1,20 @@
 '''
-Crea una classe Atleta per rappresentare le informazioni su un giocatore. 
+ES 1) Crea una classe Atleta per rappresentare le informazioni su un giocatore. 
 La classe deve contenere un attributo booleano chiamata visitaMedica.
-Aggiungi alla classe Atleta un metodo per assegnare all'atleta il nome della squadra dove gioca.
-Aggiungi alla classe Atleta un metodo chiamato effettua_visita che ponga l'attributo 
+ES 2) Aggiungi alla classe Atleta un metodo per assegnare all'atleta il nome della squadra dove gioca.
+ES 3) Aggiungi alla classe Atleta un metodo chiamato effettua_visita che ponga l'attributo 
 visitaMedica uguale a True.
-Aggiungi alla classe Atleta un metodo per visualizzare i dati del giocatore.
+ES 4) Aggiungi alla classe Atleta un metodo per visualizzare i dati del giocatore.
 '''
-class Atleta:
-    def __init__(self, name, visitaMedica, squadra):
+class Atleta: # Creo la classe
+    def __init__(self, name, squadra, visitaMedica = False): # inializzazione dell'istanza
         self.name = name
         self.squadra = squadra
-        self.visitaMedica = int(visitaMedica)
+        self.visitaMedica = visitaMedica
         print("Il giorno della sua visita medica: ", visitaMedica)
 
-    def squadra_info(self):
-        print("Gioco nella squadra:", self.squadra)
+    def squadra_info(self): # accetta 0 argomenti, ma devo passargli self
+        print("Gioco nella squadra:", self.squadra) # non RESTITUISCE niente
 
     def info(self):
         print("Dati del giocatore:", self.name)
@@ -28,9 +28,16 @@ class Atleta:
 
 name_player1 = input("Qual è il nome dell'Atleta? ")
 sq_player1 = input("Qual è il nome della sua squadra? ")
-giorno = input("Che giorno ha la visita? ")
+day = input("Che giorno ha la visita? ")
 print()
-player1 = Atleta(name_player1, giorno, sq_player1)
+player1 = Atleta(name_player1, sq_player1, day)
 player1.info()
 player1.squadra_info()
 player1.effettua_visita()
+
+name_player2 = input("Qual è il nome dell'Atleta? ")
+sq_player2 = input("Qual è il nome della sua squadra? ")
+print()
+player2 = Atleta(name = name_player2, squadra = sq_player2)
+player2.info()
+player2.squadra_info()
